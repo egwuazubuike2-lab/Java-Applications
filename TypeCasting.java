f380a7b1aa830e2508741f40cecf52e825823eed
egwuazubuike2-lab <egwuazubuike2@gmail.com>
2025-10-23 12:07:08 +0100

    Type Casting

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting{
+	public static void main(String[] args){
+	//widening
+	double num = 10;
+	System.out.printf("The value is %f",num);
+	
+	//Narrowing
+	double price = 34.89;
+	int result =(int)price;
+	System.out.printf("The value is %d%n",result);
+    }
+}
