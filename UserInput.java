367447cdc851e32d4f0ac9cd5ecc3c1e5201fd6f
egwuazubuike2-lab <egwuazubuike2@gmail.com>
2025-10-27 13:10:15 +0100

    Initial Comment

---
+++ UserInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInput{
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+		
+		double bal = 5678.98;
+		
+		System.out.print("Enter your first name: ");
+		String firstName = input.nextLine();
+		
+		System.out.printf("Hello %s",firstName);
+		System.out.print("Enter lastName: ");
+		String lastName = input.nextLine();
+		
+		System.out.print("Enter your age: ");
+		short age = input.nextShort();
+		
+		System.out.printf("Welcome %s %s to FirstBank%n",firstName,lastName);
+		System.out.printf("You are %d years old%n",age);
+		System.out.printf("Your account balance is $%.2f%n",bal);
+	}
+}
